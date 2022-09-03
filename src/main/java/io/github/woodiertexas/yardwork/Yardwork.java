b8572d869fbbe1d16d4205e0e2b2e07f6caf7713
Skidam <skidamgamer@gmail.com>
2022-09-03 12:51:07 +0200

    1.18 FABRIC PORT

--- src/main/java/io/github/woodiertexas/yardwork/Yardwork.java
+++ src/main/java/io/github/woodiertexas/yardwork/Yardwork.java
@@ -1,51 +1,25 @@
 package io.github.woodiertexas.yardwork;
 
 import io.github.woodiertexas.yardwork.items.Weedwhacker;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.recipe.SpecialRecipeSerializer;
 import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-import org.quiltmc.loader.api.ModContainer;
-import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
-import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
-import org.quiltmc.qsl.recipe.api.RecipeManagerHelper;
-import org.quiltmc.qsl.recipe.api.builder.VanillaRecipeBuilders;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Yardwork implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod name as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("Yardwork");
 
 	public static final TagKey<Block> MACHINE_HARVESTABLE = TagKey.of(Registry.BLOCK_KEY, new Identifier("yardwork", "machine_harvestable"));
 	public static final TagKey<Block> NON_MACHINE_HARVESTABLE = TagKey.of(Registry.BLOCK_KEY, new Identifier("yardwork", "non_machine_harvestable"));
-	public static SpecialRecipeSerializer<WeedwhackerRecipe> WEEDWHACKER_RECIPE;
-	public static final Weedwhacker WEEDWHACKER = new Weedwhacker(new QuiltItemSettings().group(ItemGroup.MISC).maxCount(1));
+	public static final Weedwhacker WEEDWHACKER = new Weedwhacker(new FabricItemSettings().group(ItemGroup.MISC).maxCount(1));
 
 	@Override
-	public void onInitialize(ModContainer mod) {
-		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
-
+	public void onInitialize() {
 		Registry.register(Registry.ITEM, new Identifier("yardwork", "weedwhacker"), WEEDWHACKER);
-		Registry.register(Registry.RECIPE_SERIALIZER, new Identifier("yardwork", "special_craft_weedwhacker_recipe"), new SpecialRecipeSerializer<>(WeedwhackerRecipe::new));
-
-		RecipeManagerHelper.registerStaticRecipe(
-				VanillaRecipeBuilders.shapedRecipe(
-						"B R",
-								" I ",
-								"S  ")
-						.output(new ItemStack(WEEDWHACKER))
-						.ingredient('B', Items.STONE_BUTTON)
-						.ingredient('R', Items.REDSTONE)
-						.ingredient('I', Items.IRON_INGOT)
-						.ingredient('S', Items.STRING)
-						.build(new Identifier("yardwork", "weedwhacker"), "")
-		);
 	}
 }
--- src/main/java/io/github/woodiertexas/yardwork/YardworkClient.java
+++ src/main/java/io/github/woodiertexas/yardwork/YardworkClient.java
@@ -1,31 +1,31 @@
 package io.github.woodiertexas.yardwork;
 
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.minecraft.client.item.ModelPredicateProviderRegistry;
 import net.minecraft.item.DyeableItem;
 import net.minecraft.util.Identifier;
-import org.quiltmc.loader.api.ModContainer;
-import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 
 import static io.github.woodiertexas.yardwork.Yardwork.WEEDWHACKER;
 
 public class YardworkClient implements ClientModInitializer {
-    @Override
-    public void onInitializeClient(ModContainer mod) {
-        ColorProviderRegistry.ITEM.register((stack, tintIndex) -> ((DyeableItem)stack.getItem()).getColor(stack), WEEDWHACKER);
 
-        ModelPredicateProviderRegistry.register(WEEDWHACKER, new Identifier("run"), (itemStack, world, entity, junk) -> {
-            if (entity == null) {
-                return 0.0f;
-            }
-            return entity.getActiveItem() != itemStack ? 0.0f : (itemStack.getMaxUseTime());
-        });
+	@Override
+	public void onInitializeClient() {
+		ColorProviderRegistry.ITEM.register((stack, tintIndex) -> ((DyeableItem)stack.getItem()).getColor(stack), WEEDWHACKER);
 
-        ModelPredicateProviderRegistry.register(WEEDWHACKER, new Identifier("active"), (itemStack, world, entity, junk) -> {
-            if (entity == null) {
-                return 0.0f;
-            }
-            return entity.isUsingItem() && entity.getActiveItem() != itemStack ? 1.0f : 0.0f;
-        });
-    }
+		ModelPredicateProviderRegistry.register(WEEDWHACKER, new Identifier("run"), (itemStack, world, entity, junk) -> {
+			if (entity == null) {
+				return 0.0f;
+			}
+			return entity.getActiveItem() != itemStack ? 0.0f : (itemStack.getMaxUseTime());
+		});
+
+		ModelPredicateProviderRegistry.register(WEEDWHACKER, new Identifier("active"), (itemStack, world, entity, junk) -> {
+			if (entity == null) {
+				return 0.0f;
+			}
+			return entity.isUsingItem() && entity.getActiveItem() != itemStack ? 1.0f : 0.0f;
+		});
+	}
 }
